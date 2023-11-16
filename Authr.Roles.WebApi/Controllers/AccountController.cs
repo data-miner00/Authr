abd926d43ea16f75b46efc972450d26b9f43f0df
data-miner00 <data-miner00@users.noreply.github.com>
2023-11-16 16:30:09 +0800

    feat: Added role proje

---
+++ Authr.Roles.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Authr.Roles.WebApi.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("/login")]
+        public IActionResult Login()
+        {
+            return this.SignIn(new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                        new Claim("superfluous_role", "admin"),
+                    },
+                    "cookie",
+                    nameType: null,
+                    roleType: "superfluous_role"
+                    )
+                ),
+                    authenticationScheme: "cookie");
+        }
+    }
+}
---
+++ Authr.Roles.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authr.Roles.WebApi.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public string Index() => "index";
+
+        [HttpGet("/secret")]
+        [Authorize(Roles = "admin")]
+        public string Secret() => "secret";
+    }
+}
