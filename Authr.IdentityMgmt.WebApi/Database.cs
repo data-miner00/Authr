11c9d40fb98f6f31750f7e9cf095032e81fb8e54
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-02 13:31:36 +0800

    feat: Added identity mgmt examples

---
+++ Authr.IdentityMgmt.WebApi/Database.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace Authr.IdentityMgmt.WebApi;
+
+public class Database
+{
+    private static string Hash(string str) =>
+        Convert.ToBase64String(MD5.HashData(Encoding.UTF8.GetBytes(str)));
+
+    public async Task<User?> GetUserAsync(string username)
+    {
+        var hash = Hash(username);
+        if (!File.Exists(hash))
+        {
+            return null;
+        }
+
+        await using var reader = File.OpenRead(hash);
+        return await JsonSerializer.DeserializeAsync<User>(reader);
+    }
+
+    public async Task PutAsync(User user)
+    {
+        var hash = Hash(user.Username);
+        await using var writer = File.OpenWrite(hash);
+        await JsonSerializer.SerializeAsync(writer, user);
+    }
+}
+
+public class User
+{
+    public string Username { get; set; }
+    public string HashedPassword { get; set; }
+    public List<UserClaim> Claims { get; set; } = new();
+}
+
+public record UserClaim(string Type, string Value);
---
+++ Authr.IdentityMgmt.WebApi/Program.cs
@@ -0,0 +1,148 @@
+using Authr.IdentityMgmt.WebApi;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<Database>();
+builder.Services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
+
+builder.Services.AddDataProtection();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("manager", pb =>
+    {
+        pb.RequireAuthenticatedUser()
+            .AddAuthenticationSchemes(CookieAuthenticationDefaults.AuthenticationScheme)
+            .RequireClaim("role", "manager");
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapGet("/", () => "hello world");
+
+app.MapGet("/register", async (
+    string username,
+    string password,
+    Database db,
+    IPasswordHasher<User> hasher,
+    HttpContext ctx) =>
+{
+    var user = new User
+    {
+        Username = username,
+    };
+    user.HashedPassword = hasher.HashPassword(user, password);
+    await db.PutAsync(user);
+
+    await ctx.SignInAsync(
+        CookieAuthenticationDefaults.AuthenticationScheme,
+        UserHelper.Convert(user));
+
+    return user;
+});
+
+app.MapGet("/login", async (
+    string username,
+    string password,
+    Database db,
+    IPasswordHasher<User> hasher,
+    HttpContext ctx) =>
+{
+    var user = await db.GetUserAsync(username);
+    var result = hasher.VerifyHashedPassword(user, user.HashedPassword, password);
+
+    if (result == PasswordVerificationResult.Failed)
+    {
+        return "bad credentials";
+    }
+
+    await ctx.SignInAsync(
+        CookieAuthenticationDefaults.AuthenticationScheme,
+        UserHelper.Convert(user));
+
+    return "logined";
+});
+
+app.MapGet("/promote", async (string username, Database db) =>
+{
+    var user = await db.GetUserAsync(username);
+
+    user.Claims.Add(new UserClaim("role", "manager"));
+    await db.PutAsync(user);
+    return "promoted";
+});
+
+app.MapGet("/start-reset-pw", async (string username, Database db, IDataProtectionProvider provider) =>
+{
+    var protector = provider.CreateProtector("PasswordReset");
+    var user = await db.GetUserAsync(username);
+    return protector.Protect(user.Username);
+});
+
+app.MapGet("/end-reset-pw", async (
+    string username,
+    string password,
+    string hash,
+    Database db,
+    IPasswordHasher<User> hasher,
+    IDataProtectionProvider provider) =>
+{
+    var protector = provider.CreateProtector("PasswordReset");
+    var hashUsername = protector.Unprotect(hash);
+    if (hashUsername != username)
+    {
+        return "bad hash";
+    }
+
+    var user = await db.GetUserAsync(username);
+    user.HashedPassword = hasher.HashPassword(user, password);
+
+    await db.PutAsync(user);
+
+    return "password reset";
+});
+
+
+app.MapGet("/protected", () => "something super secret").RequireAuthorization("manager");
+
+app.MapGet("/test", (UserManager<IdentityUser> userMgr, SignInManager<IdentityUser> signInMgr) =>
+{
+    // can access to userMgr and signInMgr
+});
+
+app.Run();
+
+public class UserHelper
+{
+    public static ClaimsPrincipal Convert(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new("username", user.Username),
+        };
+
+        claims.AddRange(user.Claims.Select(x => new Claim(x.Type, x.Value)));
+
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        return new(identity);
+    }
+}
