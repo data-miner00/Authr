e2bea9234dfba0b4d6bbf49f10681a21aa44c645
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-19 23:50:15 +0800

    chore: Move AuthService to Core

---
+++ Auth.Core/AuthenticationService.cs
@@ -0,0 +1,49 @@
+﻿namespace Authr.WebApi;
+
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+
+/// <summary>
+/// The sample implementation of authentication service.
+/// </summary>
+public class AuthenticationService
+{
+    private readonly IDataProtectionProvider idp;
+    private readonly IHttpContextAccessor accessor;
+    private readonly IDataProtector protector;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuthenticationService"/> class.
+    /// </summary>
+    /// <param name="idp">The <see cref="IDataProtectionProvider"/>.</param>
+    /// <param name="accessor">The <see cref="IHttpContextAccessor"/>.</param>
+    public AuthenticationService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
+    {
+        this.idp = idp;
+        this.accessor = accessor;
+        this.protector = this.idp.CreateProtector("auth-cookie");
+    }
+
+    /// <summary>
+    /// Simple sign in method that sets authentication cookies.
+    /// </summary>
+    public void SignIn()
+    {
+        this.accessor.HttpContext!.Response.Headers.SetCookie = $"auth={this.protector.Protect("usr:sharon")}";
+    }
+
+    /// <summary>
+    /// Decrypts username from authentication cookies.
+    /// </summary>
+    /// <returns>The decrypted username.</returns>
+    public string GetUsername()
+    {
+        var authCookie = this.accessor.HttpContext?.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
+        var protectedPayload = authCookie?.Split("=").Last();
+        var payload = this.protector.Unprotect(protectedPayload ?? string.Empty);
+        var parts = payload.Split(":");
+        var value = parts[1];
+
+        return value;
+    }
+}
