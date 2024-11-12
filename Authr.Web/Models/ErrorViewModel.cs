a9279687642f3d8aaede193c7763e8c130a4ef0a
data-miner00 <data-miner00@users.noreply.github.com>
2024-11-12 23:06:41 +0800

    chore: Remove EntityFramework demo

--- Authr.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Authr.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
